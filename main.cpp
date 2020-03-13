543c8ae27c268829e8c1afe21371a141a57489f8
Gabrux5 <31355000+Gabrux5@users.noreply.github.com>
2020-03-13 18:13:51 +0100

    Upload 1 preparazione

--- main.cpp
+++ main.cpp
@@ -4,49 +4,44 @@
 #include <strstream>
 
 #include "team.hxx"
+#include "translationTable.hxx"
 
 // Call and print every element
-void DisplayAllXml(std::auto_ptr<ProjectTeam> &proj) {
-	std::cout << "Team name: " << proj->TeamName() << std::endl;
-
-	ProjectTeam::Member_const_iterator it;
-	for (it = proj->Member().begin(); it != proj->Member().end(); it++) {
-		std::cout << "Name: " << it->Name() << "\nBrainPower: " << it->BrainPower() << std::endl;
+void DisplayAllXml(std::auto_ptr<translationTable> &table) {
+	translationTable::entry_iterator it;
+	for (it = table->entry().begin(); it != table->entry().end(); it++) {
+		std::cout << it->stringName() << " " << it->result() << "\n";
 	}
 }
 
-// Add a new element to a sequence
-void AddMember(std::auto_ptr<ProjectTeam> &proj) {
-	std::cout << "\n\nAdding a new member: ";
-	std::string name;
+void AddEntry(std::auto_ptr<translationTable> &table) {
+	std::string name, result;
 	std::cout << "\nName: " << std::flush;
 	std::getline(std::cin, name);
-	std::string strPower;
-	std::cout << "Brain Power: " << std::flush;
-	std::getline(std::cin, strPower);
-	int power = std::stoi(strPower);
-	Member member = Member(name, power);
-	proj->Member().push_back(member);
+	std::cout << "\nResult: " << std::flush;
+	std::getline(std::cin, result);
+	entry _entry = entry(result, name);
+	table->entry().push_back(_entry);
 }
 
 // Save to xml file
-void ToXml(std::auto_ptr<ProjectTeam> &proj, std::string path) {
+void ToXml(std::auto_ptr<translationTable> &table, std::string path) {
 	xml_schema::namespace_infomap map;
-	map["gt"].name = "team_new.xml";
-	map["gt"].schema = "library.xsd";
+	map["gt"].name = "translationTable_english_new.xml";
+	map["gt"].schema = "translationTable.xsd";
 	std::ofstream ofs(path.c_str());
-	ProjectTeam_(ofs, *proj, map);
+	translationTable_(ofs, *table, map);
 }
 
 // main function
 int main()
 {
 	try {
-		std::auto_ptr<ProjectTeam> proj(ProjectTeam_("team.xml"));
-		DisplayAllXml(proj);
-		AddMember(proj);
-		ToXml(proj, "team_new.xml");
-		DisplayAllXml(proj);
+		std::auto_ptr<translationTable> table(translationTable_("data//translationTable_english.xml"));
+		DisplayAllXml(table);
+		AddEntry(table);
+		ToXml(table, "data//translationTable_english_new.xml");
+		DisplayAllXml(table);
 	}
 	catch (const xml_schema::exception & e) {
 		std::cout << e << std::endl;
